e953c9b8a0f7a2de5b4555032a541b807f775075
Evgenii Stepanov <aerooneq@yandex.ru>
2021-03-14 11:22:10 +0300

    Solved second task

---
+++ YaProfiTask2/Controllers/Note.cs
@@ -0,0 +1,29 @@
+﻿namespace YaProfiTask2.Controllers
+{
+    public class Note
+    {
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Content { get; set;  }
+
+
+        public Note(string title, string content)
+        {
+            Id = GetNextId();
+            Title = title;
+            Content = content;
+        }
+
+        
+        private static int currentId = 0;
+        private static readonly object @lock = new object();
+        private static int GetNextId()
+        {
+            lock (@lock)
+            {
+                ++currentId;
+                return currentId;
+            }
+        }
+    }
+}
---
+++ YaProfiTask2/Controllers/NotesController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace YaProfiTask2.Controllers
+{
+    [ApiController]
+    [Route("notes")]
+    public class NotesController : ControllerBase
+    {
+        private class NoteDto
+        {
+            [JsonProperty("title")] public string NewTitle { get; set; }
+            [JsonProperty("content")] public string NewContent { get; set; }
+
+
+            public void Deconstruct(out string title, out string content)
+            {
+                title = NewTitle;
+                content = NewContent;
+            }
+        }
+        
+        private static readonly IList<Note> notes = new List<Note>();
+        
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewNote()
+        {
+            using var reader = new StreamReader(Request.Body);
+            string body = await reader.ReadToEndAsync().ConfigureAwait(false);
+            (string title, string content) = JsonConvert.DeserializeObject<NoteDto>(body);
+
+            if (content is null)
+            {
+                return new BadRequestResult();
+            }
+
+            Note newNote = new Note(title, content);
+            notes.Add(newNote);
+            return new OkObjectResult(newNote);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetNote(int id)
+        {
+            if (notes.FirstOrDefault(note => note.Id == id) is { } foundNote)
+            {
+                return new OkObjectResult(foundNote);
+            }
+
+            return new NotFoundResult();
+        }
+
+        [HttpPut("{id:int}")]
+        public  async Task<IActionResult> UpdateNote(int id)
+        {
+            using var reader = new StreamReader(Request.Body);
+            string body = await reader.ReadToEndAsync().ConfigureAwait(false);
+            (string newTitle, string newContent) = JsonConvert.DeserializeObject<NoteDto>(body);
+
+            if (notes.FirstOrDefault(note => note.Id == id) is { } foundNote)
+            {
+                if (newContent is { })
+                {
+                    foundNote.Content = newContent;
+                }
+
+                if (newTitle is { })
+                {
+                    foundNote.Title = newTitle;
+                }
+
+                return new OkResult();
+            }
+
+            return new BadRequestResult();
+        }
+
+        [HttpGet]
+        public IActionResult GetNote([FromQuery] string query)
+        {
+            if (query is null)
+            {
+                return new OkObjectResult(notes);
+            }
+
+            return new OkObjectResult(notes.Where(note => note.Title is { } && note.Title.Contains(query) ||
+                                                          note.Content is { } && note.Content.Contains(query)));
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteNote(int id)
+        {
+            if (notes.Remove(notes.FirstOrDefault(n => n.Id == id)))
+            {
+                return new OkResult();
+            }
+
+            return new BadRequestResult();
+        }
+    }
+}
